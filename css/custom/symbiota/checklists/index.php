483cd9b4f1a9711f6f0e9de2220a48816f513084
Greg Post <gregory.post@asu.edu>
2024-06-30 20:53:48 -0500

    update Custom styling

--- css/custom/symbiota/checklists/index.php
+++ css/custom/symbiota/checklists/index.php
@@ -1,6 +1,6 @@
 <?php
 include_once('../../../../config/symbini.php');
-header('Content-Type: text/html; charset=' . $CHARSET);
+header('Content-Type: text/html; charset='.$CHARSET);
 header('Location: '.$CLIENT_ROOT.'/index.php');
 ?>
 <!DOCTYPE html>
@@ -23,7 +23,7 @@ header('Location: '.$CLIENT_ROOT.'/index.php');
 				You don't have permission to access this page.
 			</div>
 			<div style="font-weight:bold;margin:10px;">
-				<a href="<?php echo htmlspecialchars($CLIENT_ROOT, ENT_COMPAT | ENT_HTML401 | ENT_SUBSTITUTE); ?>/index.php">Return to index page</a>
+				<a href="<?php echo $CLIENT_ROOT; ?>/index.php">Return to index page</a>
 			</div>
 		</div>
 		<?php
